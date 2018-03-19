3e2bc8ac2681c42b292c07ca37db9d7e701ea671
nblxanueb <a.e.pykhantsev@gmail.com>
2018-03-19 23:40:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-var recipes = {}
+var recipes = {key: 'value'}
 
 function updateObjectWithKeyAndValue(object, key, value) {
     return recipes.assign({},object,{[key}:value});
   }
 
-  
+
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  
+
 }
 
 function deleteFromObjectByKey(object, key) {
-  
+
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  
+
 }
